24f399a157e39b82f11d69c0a3071c107a96b7ba
Shaleen Shah <shaleen.shah@columbia.edu>
2019-06-22 16:56:17 -0400

    hw 9

---
+++ Pokemon API.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#6/6/2019 - soma - foundations- pokemon api
+
+
+# In[2]:
+
+
+import requests
+
+
+# In[23]:
+
+
+response = requests.get('https://pokeapi.co/api/v2/pokemon/snorlax')
+snorlax = response.json()
+
+print(snorlax)
+print[snorlax['weight']]
+
+
+# In[4]:
+
+
+print(response.json())
+
+
+# In[5]:
+
+
+type(data)
+
+
+# In[6]:
+
+
+data
+
+
+# In[7]:
+
+
+get_ipython().system('pip install requests')
+
+
+# In[8]:
+
+
+data.keys()
+
+
+# In[9]:
+
+
+data['weight'
+
+
+# In[10]:
+
+
+data['weight']
+
+
+# In[11]:
+
+
+data.values()
+
+
+# In[12]:
+
+
+response = requests.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+pikachu = response.json()
+
+
+# In[13]:
+
+
+print(data)
+
+
+# In[14]:
+
+
+data['weight']
+
+
+# In[17]:
+
+
+
+
+
+# In[18]:
+
+
+pikachu['weight']
+
+
+# In[19]:
+
+
+snorlax['weight']
+
+
+# In[ ]:
+
+
+
+
